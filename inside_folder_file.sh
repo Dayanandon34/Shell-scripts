4552d8ad2fc38a0195d203c2185f527c87b64a01
Dayanandon34 <Dayanando34@gmail.com>
2025-03-03 16:09:25 +0530

    Created new sh file

---
+++ inside_folder_file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Print a message
+echo "Hello, World!"
+
+# Create a directory
+mkdir folder_dayanand
+
+# Change into the new directory
+cd folder_dayanand
+
+# Create a new file
+touch myfile.txt
+
+# Print the contents of the directory
+echo "The files in the current directory are:" > myfile.txt
+
