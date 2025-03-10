01c692664fda2a471c253428bf63c491247d156e
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-10 22:18:17 +0530

    Update print-message.sh

--- print-message.sh
+++ print-message.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Hello world, welcome to Shell scripting"
+echo "Hello world, welcome to Shell scripting" #Its basic shell script which will desplay the output of the message.
 
 
