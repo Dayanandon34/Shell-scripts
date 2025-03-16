062bb308083c5d8e0fa6d38a7e8edc22cc94926f
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-16 09:30:15 +0530

    Update reverse-string1.sh

--- reverse-string1.sh
+++ reverse-string1.sh
@@ -5,7 +5,7 @@ input="Hello, Dayanand"
 output=""
 
 for ((i=${#input}-1; i>=0; i--)); do # for loop 
-	output="${output}${input:$i:1}" # syntax
+	output="${output}${input:$i:1}" # syntax for reverse the words
 done
 
 echo "what does output contain after execution script?"
