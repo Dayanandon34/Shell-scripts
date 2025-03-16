b2ae7acc92d2423a9c493b941ed6c6268c657f5f
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-16 09:25:55 +0530

    Update reverse-string1.sh

--- reverse-string1.sh
+++ reverse-string1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# This script help us to calculate the reverse of the sentence or text line
 input="Hello, Dayanand"
 output=""
 
-for ((i=${#input}-1; i>=0; i--)); do
-	output="${output}${input:$i:1}"
+for ((i=${#input}-1; i>=0; i--)); do # for loop 
+	output="${output}${input:$i:1}" # syntax
 done
 
 echo "what does output contain after  execution script?"
