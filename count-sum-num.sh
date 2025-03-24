e4f9dd6506de7408fbcea9843415383d5c99fe1a
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-24 21:12:43 +0530

    Update count-sum-num.sh

--- count-sum-num.sh
+++ count-sum-num.sh
@@ -2,7 +2,7 @@
 
 #This script help us to display the sum numbers
 result=0
-for (( i=1; i<=5; i++ )); do #Syntax for calculating sum
+for (( i=1; i<=5; i++ )); do #Syntax for calculating sum numbers
 	result=$((result + i))
 done
 
