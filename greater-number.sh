58390bcddd9094baadfb2018da134dfa4880802f
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-25 08:31:57 +0530

    Update greater-number.sh

--- greater-number.sh
+++ greater-number.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" -gt 10 ]; then #if synatx
 	echo "the number is greater than 10"
-elif [ "$1" -eq 10 ]; then
+elif [ "$1" -eq 10 ]; then #elif 
 	echo "the number is equal to 10"
 else
 	echo "the number is lessthan 10"
